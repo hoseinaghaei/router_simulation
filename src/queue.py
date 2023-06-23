8d9bb1125202895e48fa6e7126f9939ad7f0efd5
hossein.aghaei <hossein.aghaei@digikala.com>
2023-06-23 20:27:30 +0330

    add documentation for queues.

--- src/queue.py
+++ src/queue.py
@@ -13,6 +13,12 @@ class RouterQueue:
         pass
 
 
+"""
+   First In First Out
+   see this link https://www.geeksforgeeks.org/fifo-first-in-first-out-approach-in-programming/
+"""
+
+
 class FIFO(RouterQueue):
     def add_packet(self, packet: Packet):
         if len(self.queue) < self.limit:
@@ -23,6 +29,12 @@ class FIFO(RouterQueue):
     def pop_packet(self) -> Packet | None:
         if len(self.queue) > 0:
             return self.queue.pop(0)
+
+
+"""
+   Weighted Round Robin
+   see this link https://www.educative.io/answers/what-is-the-weighted-round-robin-load-balancing-technique
+"""
 
 
 class WRR(RouterQueue):
@@ -49,6 +61,12 @@ class WRR(RouterQueue):
             if len(self.queue[i]) > 0 and self._queue_weights[i] == 0:
                 self._queue_weights[i] = self._default_queue_weights[i] - 1
                 return self.queue[i].pop(0)
+
+
+"""
+   Non−Preemptive Priority Scheduling
+   see this link https://www.javatpoint.com/os-non-preemptive-priority-scheduling
+"""
 
 
 class NPPS(RouterQueue):
